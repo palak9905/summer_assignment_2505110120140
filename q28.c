2cec80a78126c5f1cba353280e2ad2ec1add0a6c
Palak Sharma <palak@Palaks-MacBook-Air.local>
2026-06-10 16:38:31 +0530

    main

--- q28.c
+++ q28.c
@@ -1,20 +1,22 @@
-#include<stdio.h>
-int rev=0;
-int reverse(int n)              //recursive function
-{
-    if(n==0)
-        return rev;
-    else
-    {
-        rev=rev*10+(n%10);          //concept of reverse of a number
-        return reverse(n/10);      //to call the function itself
-    }
-}
+#include <stdio.h>
+int reverse(int);
 int main()
 {
-    int num;
-    printf("Enter a number:");
-    scanf("%d",&num);
-    printf("The reverse of a number %d is %d\n",num,reverse(num));      //to print the return value of fnction
+    int n;
+    printf("Enter the number");
+    scanf("%d",&n);
+
+    printf("The reverse of number =%d",reverse(n));
     return 0;
+}
+int reverse(int n)
+{
+    static int rev =0;
+    if(n==0)
+    return rev;
+    else
+    {
+        rev = (rev*10)+(n%10);
+        return reverse(n/10);
+    }
 }
